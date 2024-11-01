d033a59ec3ff8ca1c75655fb35210061ca263c6b
Kirillsigmaa <ermolaevk998@gmail.com>
2024-11-01 14:51:12 +0300

    arhiriy5

---
+++ app/src/main/java/com/example/arhiriy5/DetailsActivity.java
@@ -0,0 +1,34 @@
+package com.example.arhiriy5;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_details);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+    public void home(View view){
+        Intent intent = new Intent(this, RecipesActivity.class);
+        startActivity(intent);
+    }
+    public void menu2(View view){
+        Intent intent = new Intent(this, DetailsActivity2.class);
+        startActivity(intent);
+    }
+}
